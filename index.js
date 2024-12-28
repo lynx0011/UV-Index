5fa6d5c3674511a1fbda9aa7ae9bde0d8bfd0f02
Andrew Del Rosario <drewulardelrosario@gmail.com>
2024-12-29 01:07:43 +0800

    Update the visuals and handle errors.

--- index.js
+++ index.js
@@ -11,12 +11,14 @@ app.use(express.static("public"));
 app.use(express.urlencoded({extended: true}));
 
 app.get("/", (req, res) => {
-    res.render("index.ejs", { object: data})
+    res.render("index.ejs", { data: data})
+    data = undefined
 });
 
 app.post("/submit", async (req, res) => {
-    let latitude = req.body.latitude
-    let longitude = req.body.longitude
+    let location = req.body.loc;
+    let latitude = parseInt(location.split(", ")[0], 10);
+    let longitude = parseInt(location.split(", ")[1], 10);
     try {
         const result = await axios.get("https://api.openuv.io/api/v1/uv?", {
             params: {
@@ -27,15 +29,48 @@ app.post("/submit", async (req, res) => {
                 "x-access-token" : apiKey
             }
         });
-        data =  {
-            uvIndex: Math.round(result.data.result.uv),
-            lat: latitude,
-            lng: longitude
-        };
+
+        let uvIndex = Math.round(result.data.result.uv);
+
+        if (uvIndex < 3) {
+            data = {
+                uv: uvIndex,
+                res: "It's low. Sunscreen is not necessary, but it's always good to protect your skin."
+            };
+
+          } else if (uvIndex >= 3 && uvIndex <= 5) {
+            data = {
+                uv: uvIndex,
+                res: "It's moderate. Consider using sunscreen if you'll be outside for extended periods."
+            };
+
+          } else if (uvIndex >= 6 && uvIndex <= 7) {
+            data = {
+                uv: uvIndex,
+                res: "It's high. Use sunscreen and seek shade during midday hours."
+            };
+
+          } else if (uvIndex >= 8 && uvIndex <= 10) {
+            data = {
+                uv: uvIndex,
+                res: "It's very high. Apply sunscreen, wear protective clothing, and avoid going out during peak hours."
+            };
+
+          } else if (uvIndex > 10) {
+            data = {
+                uv: uvIndex,
+                res: "It's extreme! Use sunscreen, wear sunglasses, cover up, and stay indoors if possible."
+            };
+
+          }
         res.redirect("/");
     } catch (err) {
+        data = {
+            message: "Error: Location is required to proceed. Please enter a valid location and try again."
+        }
+        res.render("index.ejs", { err: data})
+        data = undefined
         console.error(err.message)
-        res.render("index.ejs", { err: err.message})
     }
 });
 
